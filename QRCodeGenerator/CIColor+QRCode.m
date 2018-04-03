f75c38799ca4f05926ebdcb260f15193d3736606
Giovanni Scarrone <g.scarrone@mobiledev.it>
2018-04-03 16:58:18 +0200

    Added missing NSString category

--- QRCodeGenerator/CIColor+QRCode.m
+++ QRCodeGenerator/CIColor+QRCode.m
@@ -24,6 +24,7 @@
 //
 
 #import "CIColor+QRCode.h"
+#import "NSString+Utils.h"
 
 @implementation CIColor (QRCode)
 
--- QRCodeGenerator/QRCodeGenerator.m
+++ QRCodeGenerator/QRCodeGenerator.m
@@ -24,6 +24,7 @@
 //
 
 #import "QRCodeGenerator.h"
+#import "NSString+Utils.h"
 
 @interface QRCodeGenerator ()
 
